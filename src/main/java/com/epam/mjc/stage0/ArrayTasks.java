1aec5a1a338cfd55d35dfc617a872331570cf625
EDyugaeva <ed2408@yandex.ru>
2022-10-31 22:06:21 +0300

    commit

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -137,26 +137,19 @@ public class ArrayTasks {
         }
 
         for (int left = 0; left < arr.length; left++) {
-            // Вытаскиваем значение элемента
             int[] value = arr[left];
-            // Перемещаемся по элементам, которые перед вытащенным элементом
             int i = left - 1;
             for (; i >= 0; i--) {
-                // Если вытащили значение меньшее — передвигаем больший элемент дальше
                 if (value.length < arr[i].length) {
                     arr[i + 1] = arr[i];
                 } else {
-                    // Если вытащенный элемент больше — останавливаемся
                     break;
                 }
             }
-            // В освободившееся место вставляем вытащенное значение
             arr[i + 1] = value;
         }
 
-
         System.out.println("=====================");
-
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
